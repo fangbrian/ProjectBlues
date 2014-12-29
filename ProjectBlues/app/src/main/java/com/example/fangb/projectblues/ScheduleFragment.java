bc9b5e4d85d507f49294efdaf46abc2574453f0d
fangbrian <fangbrian@gmail.com>
2014-12-29 11:41:09 -0800

    Major refactor of ScheduleFragment

--- ProjectBlues/app/src/main/java/com/example/fangb/projectblues/ScheduleFragment.java
+++ ProjectBlues/app/src/main/java/com/example/fangb/projectblues/ScheduleFragment.java
@@ -2,26 +2,20 @@ package com.example.fangb.projectblues;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.example.fangb.projectblues.utils.DeviceUtils;
 import com.google.gson.Gson;
 
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-
-
-
 
 /**
+ * Fragment to display schedule of all hockey games
+ *
  * Created by fangb on 12/22/2014.
  */
 public class ScheduleFragment extends Fragment {
@@ -49,51 +43,17 @@ public class ScheduleFragment extends Fragment {
 
         mAdapter = new GameAdapter(getActivity());
         gameList.setAdapter(mAdapter);
-
-        parseHtmlGames();
     }
 
-    public void parseHtmlGames(){
-        prefs = this.getActivity().getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
-        //initializeNotification();
+    @Override
+    public void onStart() {
+        super.onStart();
 
-        Timer timer = new Timer ();
-        TimerTask hourlyTask = new TimerTask () {
-            @Override
-            public void run () {
-
-                if (DeviceUtils.isNetworkAvailable(getActivity())) {
-                    //Clear Saved Preferences
-                    clearSavedPreferences();
-                    //Gather Games
-                    gatherGames();
-
-
-                } else {
-                    if(prefs.contains(NEXT_GAME)){
-
-                    } else{
-
-                    }
-
-                }
-            }
-        };
-
-        //Schedule updates every hour
-        timer.schedule (hourlyTask, 0l, 1000*60*60);
-
+        refreshSchedule();
     }
 
-    private void clearSavedPreferences(){
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.clear();
-        editor.apply();
-    }
-
-    private void gatherGames() {
-
-        PointstreakParser task = new PointstreakParser() {
+    private void refreshSchedule() {
+        new PointstreakParser() {
             @Override
             protected void onPostExecute(List<Game> games) {
                 super.onPostExecute(games);
@@ -106,14 +66,16 @@ public class ScheduleFragment extends Fragment {
                     mAdapter.notifyDataSetChanged();
                     save(games);
                 }
-
-                //TODO:parse through games to see if there is a game today
-                //if internet not available use shared preferences
-                //if available used most up to date schedule
-                //TODO:if there is a game today, send notification
             }
-        };
-        task.execute();
+        }.execute();
+    }
+
+    // --- SharedPref helpers
+
+    private void clearSavedPreferences(){
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.clear();
+        editor.apply();
     }
 
     public void save(List<Game> games){
@@ -137,6 +99,7 @@ public class ScheduleFragment extends Fragment {
     }
 
     private void savePreferences(String key, String value) {
+        SharedPreferences prefs = getActivity().getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString(key, value);
         editor.apply();
