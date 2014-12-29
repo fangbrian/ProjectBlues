6f3c563db54142a7d078c29501e4fec4c01f00d5
fangbrian <fangbrian@gmail.com>
2014-12-29 11:23:50 -0800

    Add isNetworkAvailable method to DeviceUtils

--- ProjectBlues/app/src/main/java/com/example/fangb/projectblues/utils/DeviceUtils.java
+++ ProjectBlues/app/src/main/java/com/example/fangb/projectblues/utils/DeviceUtils.java
@@ -1,9 +1,26 @@
 package com.example.fangb.projectblues.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * This class contains utility methods that are specific to this device
- * 
+ *
  * Created by fangb on 12/29/2014.
  */
 public class DeviceUtils {
+
+    /**
+     * Returns true if the device currently has an internet connection
+     * @param activity
+     * @return
+     */
+    public static boolean isNetworkAvailable(Activity activity) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 }
