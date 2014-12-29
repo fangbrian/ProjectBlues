100fb254615f45fffff16039589ad7f85194c8f8
fangbrian <fangbrian@gmail.com>
2014-12-29 11:21:02 -0800

    Add DeviceUtils class

---
+++ ProjectBlues/app/src/main/java/com/example/fangb/projectblues/utils/DeviceUtils.java
@@ -0,0 +1,9 @@
+package com.example.fangb.projectblues.utils;
+
+/**
+ * This class contains utility methods that are specific to this device
+ * 
+ * Created by fangb on 12/29/2014.
+ */
+public class DeviceUtils {
+}
